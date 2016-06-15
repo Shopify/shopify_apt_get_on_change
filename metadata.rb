307bebb721cb6dd90a3f520a8bda52a7a48d8290
Elvin Efendi <elvin.efendiyev@gmail.com>
2016-06-15 13:28:28 +0200

    add missing metada items

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'On demand apt-get update'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1'
+source_url       'https://github.com/Shopify/shopify_apt_get_on_change'        if respond_to?(:source_url)
+issues_url       'https://github.com/Shopify/shopify_apt_get_on_change/issues' if respond_to?(:issues_url)
